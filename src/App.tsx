17a0a45d3c711b6609f7d4f5d74b7112c39bad2f
Abulkhaiyr <abzh423@protonmail.com>
2022-03-16 09:39:43 +0600

    Merging all of the components together
    Counter.tsx have been deleted due to the fact that it is not going to be used anywhere.
    package.json contains all of the needed dependencies for my future commits that are yet to be commited so take in account.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Counter } from './Counter'
+import { AppContainer } from './styles/styled'
+import { AddNewItem } from './components/AddNewItem'
+import './global.css'
+import { Column } from './components/Column'
+import { Card } from './components/Card'
 
 export const App = () => {
   return (
-    <>
-      <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
-    </>
+    <AppContainer>
+      <Column text="To do">
+        <Card text="Generate App Scuffold" />
+      </Column>
+      <Column text="In progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
   )
 }
